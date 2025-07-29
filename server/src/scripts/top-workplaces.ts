cb8778ceb228599c058779a5441300a15c1325e1
David Ghiurcan <david.ghiurcan2@mail.dcu.ie>
2025-07-29 20:27:11 +0100

    Implemented fetching of  data

--- server/src/scripts/top-workplaces.ts
+++ server/src/scripts/top-workplaces.ts
@@ -1,1 +1,27 @@
-console.log("TODO: Implement me!");
+const API_URL = 'http://localhost:3000/shifts';
+
+type Shift = {
+  id: number;
+  workplaceId: number;
+  workerId: number | null;
+};
+
+async function fetchData(): Promise<void>{
+   try {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+     const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+
+
+
+   }
+}
+
+
+fetchData();
